6b246ff88086b73705d549fbfa4d670dd605ed5f
Ilya Gulko <1530699+gulkily@users.noreply.github.com>
2024-03-26 23:39:08 -0400

    Create fizzbuzz.py

---
+++ fizzbuzz/fizzbuzz.py
@@ -0,0 +1,12 @@
+for x in range(1, 101):
+  if x % 15 == 0:
+    print("fizzbuzz")
+  elif x % 3 == 0:
+    print("fizz")
+  elif x % 5 == 0:
+    print("buzz")
+  else:
+    print(x)
+
+# how to run:
+# python3 fizzbuzz.py
