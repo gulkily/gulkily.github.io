891c8875a8ff3b53d5d0753341f04b15ae606039
Ilya Gulko <1530699+gulkily@users.noreply.github.com>
2024-04-03 11:15:48 -0400

    Create fizzbuzz.ts

---
+++ fizzbuzz/fizzbuzz.ts
@@ -0,0 +1,16 @@
+// classic 1-100 fizzbuzz program written in typescript:
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("fizzbuzz");
+  } else if (i % 3 === 0) {
+    console.log("fizz");
+  } else if (i % 5 === 0) {
+    console.log("buzz");
+  } else {
+    console.log(i);
+  }
+}
+
+// how to run:
+// node fizzbuzz.ts
