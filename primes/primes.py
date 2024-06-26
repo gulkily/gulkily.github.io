34bb2fc866ca7b742e2a89fa592e28e30a32871f
john smith <1334458-gulkily@users.noreply.replit.com>
2024-06-26 15:37:06 +0000

    add primes.py

---
+++ primes/primes.py
@@ -0,0 +1,14 @@
+def is_prime(n):
+  if n <= 1:
+      return False
+  for i in range(2, int(n**0.5) + 1):
+      if n % i == 0:
+          return False
+  return True
+
+print("prime numbers:")
+for i in range(1, 101):
+  if is_prime(i):
+      print(f"{i}: yes")
+  else:
+      print(f"{i}: no")
